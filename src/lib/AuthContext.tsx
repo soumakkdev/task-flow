def5a4ed492624c948fae5f284994e24a585de6c
soumakkdev <soumakkdutta@gmail.com>
2024-04-12 09:10:37 +0530

    login redirect fixes

--- src/lib/AuthContext.tsx
+++ src/lib/AuthContext.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { login } from '@/app/actions'
+import { login } from '@/actions/authActions'
 import { IAuthContext, ILoginWithPasswordParams } from '@/types/auth'
 import { ReactNode, createContext, useContext } from 'react'
 
@@ -8,16 +8,6 @@ export const AuthContext = createContext<IAuthContext>({} as IAuthContext)
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
 	async function loginWithPassword(params: ILoginWithPasswordParams) {
 		return await login(params)
-		// const response = await fetch('/api/auth/login', {
-		// 	method: 'POST',
-		// 	headers: { 'Content-Type': 'application/json' },
-		// 	body: JSON.stringify(params),
-		// })
-		// if (!response.ok) {
-		// 	throw new Error('Incorrect email or password')
-		// }
-		// const data = await response.json()
-		// return data
 	}
 
 	return <AuthContext.Provider value={{ loginWithPassword }}>{children}</AuthContext.Provider>
--- src/middleware.ts
+++ src/middleware.ts
@@ -10,6 +10,16 @@ export async function middleware(request: NextRequest) {
 	await pb.authStore.loadFromCookie(pbAuthCookie?.value ?? '')
 	isLoggedIn = pb.authStore.isValid ?? false
 
+	if (request?.nextUrl?.pathname?.startsWith('/login')) {
+		// If already logged in and the request is to go to the login page,
+		// Skip it and redirect to the home page.
+		if (isLoggedIn) {
+			return NextResponse.redirect(new URL('/', request.url))
+		}
+		return
+	}
+
+	// If not logged in then redirect to login page
 	if (!isLoggedIn) {
 		return NextResponse.redirect(new URL('/login', request.url))
 	}
@@ -18,5 +28,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-	matcher: '/',
+	matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
 }
